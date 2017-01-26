5d33e65c47bde255e6703d22f565a83fb36037b3
Vera Zou <zouvra@gmail.com>
2017-01-26 10:13:52 -0800

    interactive/node

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+	var nameToShow = req.params.userName;
+	if (nameToShow === undefined){
+		nameToShow = 'World'
+	}
+
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -28,6 +28,6 @@ function projectClick(e){
 	if (description.length == 0){
 		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
 	}else{
-		description.fadeOut();
+		description.toggle();
 	}
 }
